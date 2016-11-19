fcd69216631b28490176445a33291f45be90e00a
cahu <cahu@gmx.us>
2016-11-19 13:34:39 +0100

    some documentation

--- src/KRPCHS.hs
+++ src/KRPCHS.hs
@@ -83,6 +83,10 @@ streamHandshake sock clientId = do
         err                 -> fail (show err)
 
 
+-- | @'withRPCClient' name host port func@ opens a connection to the KRPC server and
+-- pass the resulting 'RPCClient' handle to the specified function @func@.
+-- When @func@ returns (or if it throws an exception), the connection with the
+-- KRPC server is automatically closed.
 withRPCClient :: String -> HostName -> ServiceName -> (RPCClient -> IO a) -> IO a
 withRPCClient name host port func = do
     sock <- getSocket host port
@@ -93,6 +97,10 @@ withRPCClient name host port func = do
         (close sock)
 
 
+-- | @'withStreamClient' rpcClient host port func@ opens a connection to the
+-- KRPC streams server and pass the resulting 'StreamClient' handle to the
+-- specified function @func@. When @func@ returns (or if it throws an
+-- exception), the connection with the Stream server is automatically closed.
 withStreamClient :: RPCClient -> HostName -> ServiceName -> (StreamClient -> IO a) -> IO a
 withStreamClient RPCClient{..} host port func = do
     sock <- getSocket host port
@@ -102,29 +110,43 @@ withStreamClient RPCClient{..} host port func = do
         (close sock)
 
 
+-- | @'runRPCProg' client ctx@ runs the 'RPCContext' @ctx@ using the provided
+-- 'RPCClient' @client@.
 runRPCProg :: RPCClient -> RPCContext a -> IO a
 runRPCProg client ctx = runReaderT (runRPCContext ctx) client
 
 
+-- | @'getStreamMessage' client@ extracts the next 'KRPCStreamMsg' received by
+-- the provided 'StreamClient'.
 getStreamMessage :: StreamClient -> RPCContext KRPCStreamMsg
 getStreamMessage client = getStreamMessageIO client >>= either throwM return
 
 
+-- | A generalized version of 'getStreamMessage' to be used in any 'MonadIO'.
 getStreamMessageIO :: MonadIO m => StreamClient -> m (Either ProtocolError KRPCStreamMsg)
 getStreamMessageIO StreamClient{..} = (fmap unpackStreamMsg) <$> liftIO (recvMsg streamSocket)
   where
     unpackStreamMsg res = KRPCStreamMsg $ M.fromList (extractStreamMessage res)
 
 
+-- | @'messageResultsCount' m@ returns the number of messages contained in the
+-- 'KRPCStreamMsg' m.
 messageResultsCount :: KRPCStreamMsg -> Int
 messageResultsCount = M.size . streamMsg
 
 
+-- | @'messageHasResultFor' s m@ returns true if the @KRPCStreamMsg@ contains a
+-- result for the 'KRPCStream' @s@. It is then safe to call @'getStreamResult'
+-- s m@.
 messageHasResultFor :: KRPCStream a -> KRPCStreamMsg -> Bool
 messageHasResultFor KRPCStream{..} KRPCStreamMsg{..} =
     M.member streamId streamMsg
 
 
+-- | @'getStreamResult' s m@ extracts the result of a 'KRPCStream' @s@ from the
+-- 'KRPCStreamMsg' @m@. If no such result exist, an exception is thrown. You
+-- can check whether it is safe to call this function wiht
+-- ''messageHasResultFor'.
 getStreamResult :: (KRPCResponseExtractable a) => KRPCStream a -> KRPCStreamMsg -> RPCContext a
 getStreamResult KRPCStream{..} KRPCStreamMsg{..} =
     maybe (throwM NoSuchStream)
@@ -132,16 +154,22 @@ getStreamResult KRPCStream{..} KRPCStreamMsg{..} =
           (M.lookup streamId streamMsg)
 
 
+-- | Requests the creation of a new 'KRPCStream' using the provided 'KRPCStreamReq'.
 addStream :: (KRPCResponseExtractable a) => KRPCStreamReq a -> RPCContext (KRPCStream a)
 addStream = requestStream
 
 
+-- | Requests the removal of a stream.
 removeStream :: KRPCStream a -> RPCContext ()
 removeStream KRPCStream{..} = do
     resp <- sendRequest (makeRequest "KRPC" "RemoveStream" [ makeArgument 0 streamId ])
     processResponse resp
 
 
+-- | @'withStream' r f@ requests the creation of a new stream using the
+-- 'KRPCStreamReq' @r@ and pass the resulting 'KRPCStream' to @f@. When @f@
+-- returns or throws, a request to remove the stream will be sent to the KRPC
+-- server automatically.
 withStream :: KRPCResponseExtractable a => KRPCStreamReq a -> (KRPCStream a -> RPCContext b) -> RPCContext b
 withStream r f = mask $ \restore -> do
     s <- addStream r
